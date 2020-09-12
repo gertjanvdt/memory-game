24b4459a7d3a3b23ecc41d7d87dd1a815007b367
gertjanvdt <64344880+gertjanvdt@users.noreply.github.com>
2020-09-12 20:54:02 +0800

    update

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
+// BUTTONS
+const nextButton = document.getElementById("next");
+const nextTurn = document.getElementById("next-try");
+const backButton = document.getElementById("back");
+const startButton = document.getElementById("start");
+
+// DOM ELEMENTS
 const cardContainer = document.querySelector(".card-container");
 const cards = document.getElementsByClassName("card");
 const images = document.getElementsByTagName("img");
-let player1Score = document.getElementById("player1-score");
-let resultText = document.querySelector(".message");
 const overlay = document.querySelector(".message-container");
-const nextTurn = document.getElementById("next-try");
 let playerInput = document.getElementById("player-amount").value;
-const nextButton = document.getElementById("next");
 const step1 = document.getElementById("step1");
 const step2 = document.getElementById("step2");
+const playerNamesContainer = document.getElementById("player-names-container");
+const startScreen = document.querySelector(".start-screen");
+
+// GAME VARIABLES
+let player1Score = document.getElementById("player1-score");
+let resultText = document.querySelector(".message");
 let imagesClicked = 0;
 let playerScore = 0;
 let correctGuesses = 0;
@@ -164,40 +173,105 @@ document.createElement(setRandomImage());
 // Phase 2
 // ===============
 
-// 1) Choose amount of players and add name
-
-nextButton.addEventListener("click", (e) => {
-  playerInput = document.getElementById("player-amount").value;
-  if (playerInput !== "select") {
-    step1.classList.add("hidden");
-    step2.classList.remove("hidden");
-    setPLayerInput();
-  } else {
-    const warning = document.createElement("h3");
-    warning.innerHTML = "PLEASE SELECT A NUMBER OF PLAYERS";
-    warning.classList = "warning";
-    step1.appendChild(warning);
-  }
-});
-
 // ***** FUNCTIONS ******
+
+// 1) Choose amount of players and add name
 
 // Input player name up to 4 players
 function setPLayerInput() {
   for (let i = 0; i < parseInt(playerInput); i++) {
     let input = document.createElement("input");
     input.type = "text";
+    input.className = "playerName";
     input.placeholder = `Name of player ${i + 1}`;
-    const playerNames = document.getElementById("player-names");
-    playerNames.appendChild(input);
+    playerNamesContainer.appendChild(input);
   }
 }
 
-// Back button go back to player selection
+function setWarning() {
+  const warning = document.createElement("h3");
+  warning.innerHTML = "PLEASE SELECT A NUMBER OF PLAYERS";
+  warning.classList = "warning";
+  step1.appendChild(warning);
+}
 
-// Check if players selected, otherwise display error
+const warningIsSet = () => {
+  const warning = document.querySelector(".warning");
+  if (warning === null) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+function removePlayerInputs() {
+  while (playerNamesContainer.hasChildNodes()) {
+    playerNamesContainer.removeChild(playerNamesContainer.firstChild);
+  }
+}
 
 // Rotate turns with up to 4 players
+class Player {
+  constructor(playerName, playerScore, isTurn, isWinner) {
+    this.playerName = playerName;
+    this.playerScore = playerScore;
+    this.isTurn = isTurn;
+    this.isWinner = isWinner;
+  }
+}
+// Function to create the players in the game after input
+const playersObject = () => {
+  const playerInputs = document.getElementsByClassName("playerName");
+  const gamePlayers = [];
+  for (let i = 0; i < playerInputs.length; i++) {
+    gamePlayers.push(new Player(playerInputs[i].value, 0, false, false));
+  }
+  return gamePlayers;
+};
+
+function createPlayersScreen(players) {
+  playerContainer = document.querySelector(".player-board-container");
+  console.log(players);
+  for (let i = 0; i < players.length; i++) {
+    const div = document.createElement("div");
+    div.classList.add("player");
+    playerContainer.appendChild(div);
+    const h2 = document.createElement("h2");
+    const h3 = document.createElement("h3");
+    h2.innerHTML = players[i].playerName;
+    h3.innerHTML = `Score: <span id="player${i + 1}-score">0</span>`;
+    div.appendChild(h2);
+    div.appendChild(h3);
+  }
+}
+
+// ***** EVENT LISTENERS *****
+
+// When player chooses amount of players and goes to next step
+nextButton.addEventListener("click", (e) => {
+  playerInput = document.getElementById("player-amount").value;
+  if (playerInput !== "select") {
+    step1.classList.add("hidden");
+    step2.classList.remove("hidden");
+    setPLayerInput();
+    // Check if players selected, otherwise display error
+    // fix the issue that warning appear multiple times when clicking next
+  } else if (warningIsSet() === false) {
+    setWarning();
+  }
+});
+
+// Back button go back to player selection
+backButton.addEventListener("click", (e) => {
+  step1.classList.remove("hidden");
+  step2.classList.add("hidden");
+  removePlayerInputs();
+});
+
+startButton.addEventListener("click", (e) => {
+  createPlayersScreen(playersObject());
+  startScreen.classList.add("hidden");
+});
 
 // Phase 3
 // Ad max amount of tries before failing
